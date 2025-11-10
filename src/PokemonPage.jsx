b1a87312da60b5351f8737e09982a39e0e824448
Raine Tikkanen <raine.tikkan3n@gmail.com>
2025-11-10 12:10:41 +0200

    11.8 Back to green

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -30,12 +30,14 @@ const PokemonPage = ({ previous, next }) => {
   console.log('hiddenAbility=', hiddenAbility)
   /* eslint-disable no-console */
 
+  console.log(previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>)
+
   return (
     <>
       <div className="links">
-        {previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>}
+        {previous ? <Link to={`/pokemon/${previous.name}`}>Previous</Link> : <span></span>}
         <Link to="/">Home</Link>
-        {next && <Link to={`/pokemon/${previous.name}`}>Next</Link>}
+        {next ? <Link to={`/pokemon/${next.name}`}>Next</Link> : <span></span>}
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
         <div className="pokemon-image" style={{ backgroundImage: `url(${pokemon.sprites.front_default})` }} />

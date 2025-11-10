d56a9a299d2357f5bcfe9af10f7b529d31c2ef72
Raine Tikkanen <raine.tikkan3n@gmail.com>
2025-11-10 12:15:51 +0200

    11.8 Back to green

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -35,9 +35,9 @@ const PokemonPage = ({ previous, next }) => {
   return (
     <>
       <div className="links">
-        {previous ? <Link to={`/pokemon/${previous.name}`}>Previous</Link> : <span></span>}
+        {previous ? <Link to={`/pokemon/${previous.name}`}>Previous</Link> : null}
         <Link to="/">Home</Link>
-        {next ? <Link to={`/pokemon/${next.name}`}>Next</Link> : <span></span>}
+        {next ? <Link to={`/pokemon/${next.name}`}>Next</Link> : null}
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
         <div className="pokemon-image" style={{ backgroundImage: `url(${pokemon.sprites.front_default})` }} />
